14c1cebbdea0ba7f27b3a400721cf1b9fa80005e
omar-437 <66955352+omar-437@users.noreply.github.com>
2020-06-27 20:28:43 +0300

    تم معا البونس

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,47 @@
 // السؤال الأول
 // 1
+const NAME =  "Omar Albusiri" ;
 
 // 2
-
+let counter = 17;
 // 3 and 4
+console.log("انا " + name + 'عمري ' +  counter);
 
 // 5 and 6
+console.log(counter*5);
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+const Myhobbies = ['Football',"Tanes","swimming"];
 
 // 2
-
+Myhobbies.push(counter);
+console.log(Myhobbies);
 // 3
-
+Myhobbies.pop();
+console.log(Myhobbies);
 // 4
+let Leng= { 
+Languge: 'Javascirbt' ,
+path: "WEB"
+
+};
+
+
 
 // 5
-
+const Techers= ["Hussein Al-Mousawi","Rahaf Alenez","Moudi Al Bannai","Nancy El-Sharkaw"];
 // 6
-
+console.log(Techers.NAME);
 // 7
-
+console.log("Hussein Al-Mousawi");
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
-
+Techers = "Live Is good";
 // 2
-
+console.log(Techers)
 // console results => console.png
